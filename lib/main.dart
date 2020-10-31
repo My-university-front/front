7ab8c49684010a4b2859458e1933997d50d53860
Sayed_Mahdi Razavi <razavismhr@gmail.com>
2020-10-31 17:05:57 +0330

    Add_Login_Screen

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:my_university/screens/email_verfication_screen.dart';
+import 'package:my_university/screens/home_screen.dart';
+import 'package:my_university/screens/login_screen.dart';
+import 'package:my_university/screens/registeration_screen.dart';
+import 'package:my_university/screens/welcome_screen.dart';
+
+import 'constants.dart';
+import 'screens/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Auth',
+      theme: ThemeData(
+        accentColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      initialRoute: SplashScreen.id,
+      routes: {
+        RegisterationScreen.id: (context) => RegisterationScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        SplashScreen.id: (context) => SplashScreen(),
+        EmailVerificationScreen.id: (context) => EmailVerificationScreen(),
+        HomeScreen.id: (context) => HomeScreen(),
+      },
+    );
+  }
+}
+
